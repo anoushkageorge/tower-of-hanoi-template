281c4dd8fb2c0d649dcaab85667afe51d347ec77
Anoushka George <122048715+anoushkageorge@users.noreply.github.com>
2025-03-29 06:32:24 +0000

    final

--- TowerSolver.java
+++ TowerSolver.java
@@ -1,4 +1,3 @@
-
 public class TowerSolver {
     private TowerModel model;
 
@@ -10,23 +9,16 @@ public class TowerSolver {
     public void solve(TowerModel model)
     {
         this.model = model;
-        solve(model.height(), 0, 2, 1); //3 rods
-        // Call the missing solve method (not this one)
+        solve(model.height(), 0, 2, 1); // Solve for all disks
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
-    private void solve (int n, int source, int destination, int auxillerary) {
-        if (n==1) {
-            move.move(source, destination); //base case
+    private void solve(int n, int source, int destination, int auxiliary) {
+        if (n == 1) {
+            model.move(source, destination); // Base case
         } else {
-            solve(n-1, source, auxillerary, destination); //movel n-1 disk source to auxillerary
-            model.move(source, destination); // nth from srouce to destination
-            solve(n-1, auxillerary, destination, source); // move disk from aux to destination
+            solve(n - 1, source, auxiliary, destination); // Move n-1 disks from source to auxiliary
+            model.move(source, destination); // Move nth disk from source to destination
+            solve(n - 1, auxiliary, destination, source); // Move disks from auxiliary to destination
+        }
     }
-
-}
 }
